1816f407f590287705ead52d1c3738fcbbbb3e6f
Zaidanfzn <zaidanfauzan.edu@gmail.com>
2024-08-12 19:03:50 +0700

    Update content JavaScipt file test

--- Just-Test/test.js
+++ Just-Test/test.js
@@ -1,13 +1,30 @@
-const contoh = document.querySelectorAll('.coba');
+// const contoh = document.querySelectorAll('.coba');
 
-contoh.forEach(element => {
+// contoh.forEach(element => {
+//     element.addEventListener('mouseover', () => {
+//         element.style.color = 'green';
+//         element.style.cursor = 'pointer';
+//         element.textContent = 'HELLO ACADEMICIAN';
+//     });
+//     element.addEventListener('mouseout', () => {
+//         element.style.color = 'red';
+//         element.textContent = 'HELLO WORLD';
+//     });
+// });
+
+const contoh = document.getElementsByClassName('coba');
+
+for (let i = 0; i < contoh.length; i++) {
+    const element = contoh[i];
+
     element.addEventListener('mouseover', () => {
         element.style.color = 'green';
         element.style.cursor = 'pointer';
         element.textContent = 'HELLO ACADEMICIAN';
     });
+
     element.addEventListener('mouseout', () => {
         element.style.color = 'red';
         element.textContent = 'HELLO WORLD';
     });
-});
+}
